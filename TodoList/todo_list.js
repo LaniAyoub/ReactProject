6aa183b4d8f64a54fb1376dd23fb9b054c087bb8
LaniAyoub <ayoub.lani@horizon-tech.tn>
2025-02-07 23:19:41 +0100

    .\todo_list.Create a To-Do List Using JavaScript

---
+++ TodoList/todo_list.js
@@ -0,0 +1,42 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
+const taskList = document.getElementById("taskList");
+
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+let tasks = [];
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+        tasks.push({ text: taskText});
+        taskInput.value = "";
+        displayTasks();
+    }
+}
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+}
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+displayTasks();
+function clearAllTasks()
+{
+    tasks.splice(0,tasks.length);
+    displayTasks();
+}
+addTaskBtn.addEventListener("click", addTask);
+clearAllBtn.addEventListener("click", clearAllTasks);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
